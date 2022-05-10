3872fdfd2d80f5ea91a526dd0c1cee70c9881533
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-05-10 13:08:46 +0300

    added source class

--- app/models.py
+++ app/models.py
@@ -10,11 +10,3 @@ class Article:
         
 
         
-class Source:
-    def __init__(self,id,name,description,url,language):
-        self.id = id
-        self.name = name
-        self.descripton = description
-        self.url = url
-        self.language = language
-    
