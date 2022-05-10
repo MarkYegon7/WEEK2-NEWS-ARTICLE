0efc2d748f71deed795e05518b698bd3883a8328
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-05-10 13:20:16 +0300

    store base_url

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Config:
     '''
     NEWS_API_KEY = '43728a6ff9584c9baa59768c05c80b37'
     SECRET_KEY = '191743892af9cad7b309cd56ee66ad46'
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
-                        
+    
 
 
 class ProdConfig(Config):
