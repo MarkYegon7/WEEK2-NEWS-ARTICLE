256c54b52000eb3925a7000d3c9764b7592a185e
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-05-10 13:10:18 +0300

    add license

--- app/models.py
+++ app/models.py
@@ -1,4 +1,13 @@
-
+class Article:
+    
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+        
 
         
 class Source:
